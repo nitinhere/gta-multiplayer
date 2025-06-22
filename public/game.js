9f235d0324fd1f7c7a55d01359894ae5e6928beb
nitinhere <nitinsharma2706@gmail.com>
2025-06-22 23:04:11 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,20 +1,28 @@
-
 const socket = io();
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-let playerId;
 let players = {};
+let myId = null;
 
-const keys = {};
-const speed = 3;
-const bullets = [];
+document.addEventListener("keydown", (e) => {
+  const me = players[myId];
+  if (!me) return;
 
-window.addEventListener("keydown", (e) => keys[e.key] = true);
-window.addEventListener("keyup", (e) => keys[e.key] = false);
+  if (e.key === "ArrowUp") me.y -= 5;
+  if (e.key === "ArrowDown") me.y += 5;
+  if (e.key === "ArrowLeft") me.x -= 5;
+  if (e.key === "ArrowRight") me.x += 5;
+  if (e.key === " ") {
+    const bullet = { x: me.x + 10, y: me.y };
+    socket.emit("shoot", bullet);
+  }
+
+  socket.emit("move", { x: me.x, y: me.y });
+});
 
 socket.on("init", (data) => {
-  playerId = data.id;
+  myId = data.id;
   players = data.players;
 });
 
@@ -22,68 +30,30 @@ socket.on("newPlayer", (data) => {
   players[data.id] = { x: data.x, y: data.y, bullets: [] };
 });
 
-socket.on("updatePlayers", (data) => {
-  players = data;
+socket.on("updatePlayers", (updatedPlayers) => {
+  players = updatedPlayers;
+});
+
+socket.on("bulletFired", ({ id, bullet }) => {
+  if (players[id]) players[id].bullets.push(bullet);
 });
 
 socket.on("removePlayer", (id) => {
   delete players[id];
 });
 
-socket.on("bulletFired", ({ id, bullet }) => {
-  if (players[id]) {
-    players[id].bullets.push(bullet);
-  }
-});
-
-function shoot() {
-  const player = players[playerId];
-  const bullet = { x: player.x + 20, y: player.y + 10, dx: 5, dy: 0 };
-  bullets.push(bullet);
-  socket.emit("shoot", bullet);
-}
-
-function update() {
-  const p = players[playerId];
-  if (!p) return;
-
-  if (keys["ArrowUp"]) p.y -= speed;
-  if (keys["ArrowDown"]) p.y += speed;
-  if (keys["ArrowLeft"]) p.x -= speed;
-  if (keys["ArrowRight"]) p.x += speed;
-  if (keys[" "]) shoot();
-
-  socket.emit("move", { x: p.x, y: p.y });
-
-  bullets.forEach(b => b.x += b.dx);
-}
-
-function draw() {
+function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw buildings
-  ctx.fillStyle = "#222";
-  ctx.fillRect(200, 100, 100, 100);
-  ctx.fillRect(500, 300, 120, 120);
-
-  // Draw players
   for (let id in players) {
     const p = players[id];
-    ctx.fillStyle = id === playerId ? "red" : "blue";
-    ctx.fillRect(p.x, p.y, 40, 20);
-
-    // Draw bullets
-    ctx.fillStyle = "yellow";
-    (p.bullets || []).forEach(b => ctx.fillRect(b.x, b.y, 5, 5));
+    ctx.fillStyle = id === myId ? "blue" : "red";
+    ctx.fillRect(p.x, p.y, 20, 20);
+    ctx.fillStyle = "black";
+    p.bullets.forEach(b => {
+      b.x += 5;
+      ctx.fillRect(b.x, b.y + 8, 5, 5);
+    });
   }
-
-  // Local bullets
-  bullets.forEach(b => ctx.fillRect(b.x, b.y, 5, 5));
-}
-
-function gameLoop() {
-  update();
-  draw();
   requestAnimationFrame(gameLoop);
 }
 
