c6524b94a455c0893e2a926a59b01423bd3c1f61
nitinhere <nitinsharma2706@gmail.com>
2025-06-22 23:24:56 +0530

    Cleaned up merge conflicts and improved game.js

--- public/game.js
+++ public/game.js
@@ -5,7 +5,6 @@ const ctx = canvas.getContext("2d");
 let players = {};
 let myId = null;
 
-HEAD
 // Movement controls
 const keys = {
   ArrowUp: false,
@@ -79,23 +78,6 @@ function gameLoop() {
   requestAnimationFrame(gameLoop);
 }
 
-document.addEventListener("keydown", (e) => {
-  const me = players[myId];
-  if (!me) return;
-
-  if (e.key  "ArrowUp") me.y -= 5;
-  if (e.key  "ArrowDown") me.y += 5;
-  if (e.key "ArrowLeft") me.x -= 5;
-  if (e.key  "ArrowRight") me.x += 5;
-  if (e.key  " ") {
-    const bullet = { x: me.x + 10, y: me.y };
-    socket.emit("shoot", bullet);
-  }
-
-  socket.emit("move", { x: me.x, y: me.y });
-});
-9f235d0324fd1f7c7a55d01359894ae5e6928beb
-
 socket.on("init", (data) => {
   myId = data.id;
   players = data.players;
@@ -105,7 +87,6 @@ socket.on("newPlayer", (data) => {
   players[data.id] = { x: data.x, y: data.y, bullets: [] };
 });
 
-HEAD
 socket.on("updatePlayers", (updated) => {
   players = updated;
 });
@@ -115,36 +96,10 @@ socket.on("bulletFired", ({ id, bullet }) => {
     players[id].bullets = players[id].bullets || [];
     players[id].bullets.push(bullet);
   }
-
-socket.on("updatePlayers", (updatedPlayers) => {
-  players = updatedPlayers;
-});
-
-socket.on("bulletFired", ({ id, bullet }) => {
-  if (players[id]) players[id].bullets.push(bullet);
-9f235d0324fd1f7c7a55d01359894ae5e6928beb
 });
 
 socket.on("removePlayer", (id) => {
   delete players[id];
 });
 
-HEAD
-
-function gameLoop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let id in players) {
-    const p = players[id];
-    ctx.fillStyle = id  myId ? "blue" : "red";
-    ctx.fillRect(p.x, p.y, 20, 20);
-    ctx.fillStyle = "black";
-    p.bullets.forEach(b => {
-      b.x += 5;
-      ctx.fillRect(b.x, b.y + 8, 5, 5);
-    });
-  }
-  requestAnimationFrame(gameLoop);
-}
-
-9f235d0324fd1f7c7a55d01359894ae5e6928beb
 gameLoop();
